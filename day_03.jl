0d9becb5bca2c88d7be0d8d579e268b79ac65c2a
Matt Heiden <MHeiden2@munichre.com>
2024-12-03 14:49:38 -0500

    Stylistic edits

--- day_03.jl
+++ day_03.jl
@@ -1,21 +1,20 @@
 # Part 1
-lines = readlines("./puzzle_inputs/day_03.txt")
+text = readlines("./puzzle_inputs/day_03.txt") |> join
 regex = r"(?<=mul\()\d+,\d+(?=\))"
 
-function parse_line(line, regex)
-    pairs = [x.match for x in eachmatch(regex, line)]
+function parse_text(text, regex)
+    pairs = [x.match for x in eachmatch(regex, text)]
     pairs = [parse.(Int, pair) for pair in split.(pairs, ",")]
     return pairs # Vector{Vector{Int}}
 end
 
-pairs = vcat(parse_line.(lines, regex)...)
+pairs = parse_text(text, regex)
 part1_answer = sum(x * y for (x, y) in pairs)
 println("Part 1 answer: $part1_answer")
 
 # Part 2 - Find the deactivated instructions and subtract from part 1 answer
-text = join(lines)
 regex2 = r"don't().*?(do\(\)|$)"
 deactivated_text = [x.match for x in eachmatch(regex2, text)] |> join
-deactivated_pairs = parse_line(deactivated_text, regex)
+deactivated_pairs = parse_text(deactivated_text, regex)
 part2_answer = part1_answer - sum(x * y for (x, y) in deactivated_pairs)
 println("Part 2 answer: $part2_answer")
